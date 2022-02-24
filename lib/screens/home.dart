7310a2798e34f0ecba38b7e3c6361e6e5a9e51d2
l3limp <kakarvardaan@gmail.com>
2022-02-25 00:37:38 +0530

    added logo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -18,6 +18,11 @@ class _HomeState extends State<Home> {
       end: Alignment.bottomCenter,
       colors: <Color>[Color(0xFF264CAA), Color(0xFFCF85D7)],
     ).createShader(const Rect.fromLTWH(50.0, 100.0, 200.0, 250.0));
+    final Shader linearGradient2 = const LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: <Color>[Color(0xFF264CAA), Color(0xFFCF85D7)],
+    ).createShader(const Rect.fromLTWH(10.0, 200.0, 200.0, 250.0));
 
     return Scaffold(
         body: SizedBox(
@@ -33,7 +38,7 @@ class _HomeState extends State<Home> {
           ),
         ),
         Positioned(
-          top: _height * 0.3,
+          top: _height * 0.27,
           child: SizedBox(
             width: _width,
             child: Container(
@@ -46,22 +51,36 @@ class _HomeState extends State<Home> {
                         fontFamily: "Dela",
                         fontWeight: FontWeight.w400,
                         foreground: Paint()..shader = linearGradient)),
-                const Text("Website Launching Soon ...",
+                Text("Website Launching Soon ...",
                     style: TextStyle(
                         fontFamily: "Goth",
-                        color: Colors.indigo,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400)),
+                        fontSize: 22,
+                        fontWeight: FontWeight.w100,
+                        foreground: Paint()..shader = linearGradient2)),
               ]),
             ),
           ),
         ),
         Positioned(
-          bottom: _height * 0.05,
+          bottom: _height * 0.08,
           child: SizedBox(
             width: _width,
             height: _height * 0.3,
             child: const StatsBar(),
+          ),
+        ),
+        Positioned(
+          top: _height * 0.02,
+          left: _width * 0.01,
+          child: Container(
+            decoration: const BoxDecoration(
+                boxShadow: [BoxShadow(color: Colors.white, blurRadius: 70)]),
+            height: 70,
+            width: 120,
+            child: Image.asset(
+              "assets/images/light_cel_2.png",
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ]),
